6067879f1b148bf93a293af812aeb4b4d6d50732
JeffHenryson <jeffhunbruey@hotmail.com>
2026-06-01 21:08:15 -0300

    feat: Google OAuth GIS, specs DEV, banner email pendente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
+  googleClientId: '272358372118-v3nf24o331kfpg0n3gqm0497o1fd9faf.apps.googleusercontent.com',
   sentryDsn: '',
 };
